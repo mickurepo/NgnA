d7d25d83a9125121b7aa95f8ee11fb2a019cd654
mickurepo <michalk30@wp.pl>
2021-03-22 16:14:09 +0100

    - correction in target-name in script files

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 cd target
-java -jar NgnA-0.0.1-SNAPSHOT.jar
+java -jar NgnA-1.0-SNAPSHOT.jar
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd target
-java -jar NgnA-0.0.1-SNAPSHOT.jar
+java -jar NgnA-1.0-SNAPSHOT.jar
